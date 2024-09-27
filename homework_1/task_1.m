134047619a57f4ae6c846db14e7def23104b0feb
lalaisthebest123 <kevincole921@gmail.com>
2024-09-27 15:40:53 +0000

    we solved the first task, pat on the back for me

---
+++ homework_1/task_1.m
@@ -0,0 +1,13 @@
+% Author Name: Kevin Cole
+% Email: kevincole921@gmail.com
+% Course: MATLAB Progamming - Fall 2024
+% Assignement: Homework 1
+
+% 1. Varibale Operations(20 points)
+% Creating Variables
+r = 5 %Radius of the circle
+%Calculation of Area of the Circle
+a = ... %Area of Circle
+% c) Calculate the circumference of the circle and store it in a variable called 'circumference'
+% Calculatiing the circumference of the circle
+c = ... %
